83f060d4959ef57e556d7d2936f711ddd4d3669d
A. Berkay Bal <36015894+akinizer@users.noreply.github.com>
2020-12-29 00:25:45 +0300

    https://github.com/akinizer/KoroVeri.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link, Switch, Route } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import { CaseModelList, AddCaseModel, CaseModel } from "react"
+
+import {MainCase,AllCases,CityCases,DateCases, AddCases} from './components/Main'
+
+export default class App extends Component{
+  render(){
+    return(
+     <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/"} className="nav-link">
+                <h5>CaseModel</h5>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/all"} className="nav-link">
+                All
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+			<li className="nav-item">
+              <Link to={"/city"} className="nav-link">
+                City
+              </Link>
+            </li>
+
+			<li className="nav-item">
+              <Link to={"/date"} className="nav-link">
+                Date
+              </Link>
+            </li>
+          </div>
+        </nav>
+       <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/"]} component={MainCase} />
+            <Route exact path={["/all"]} component={AllCases} />
+            <Route 
+              exact path="/city/:attr"
+              children={({ match }) => match && <CityCases match={match} />}
+            />
+
+            <Route 
+              exact path={["/date/:attr"]} component={DateCases}
+               children={({ match }) => match && <DateCases match={match} />}/>
+            <Route path={["/add"]} component={AddCases} />
+          </Switch>
+        </div>
+      </div>
+  )
+  }
 }
-
-export default App;
